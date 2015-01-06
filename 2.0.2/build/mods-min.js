b4f3c444a2dc32aa85ea6d671254178f0e9b27a5
kissyteam <tiehang.lth@taobao.com>
2015-01-06 16:15:26 +0800

    sync daily version 2.0.2

--- 2.0.2/build/mods-min.js
+++ 2.0.2/build/mods-min.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-/*!build time : 2015-01-05 4:45:25 PM*/
-KISSY.config("modules",{"kg/xplayer/2.0.1/mini":{requires:["base"]}});
-=======
-/*!build time : 2015-01-06 3:48:50 PM*/
+/*!build time : 2015-01-06 4:13:38 PM*/
 KISSY.config("modules",{"kg/xplayer/2.0.2/mini":{requires:["base"]}});
->>>>>>> 38a62eb2f861f8ed0c4ab5098ec99a3f8164f50a
