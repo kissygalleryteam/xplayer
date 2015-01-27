58d34ac3fee43c90b3864372ca233b8a65052dc0
kissyteam <tiehang.lth@taobao.com>
2015-01-27 10:01:56 +0800

    sync doc automatically by kpm

--- doc/build/mods-min.js
+++ doc/build/mods-min.js
@@ -1,2 +1,2 @@
-/*!build time : 2015-01-06 4:13:38 PM*/
-KISSY.config("modules",{"kg/xplayer/2.0.2/mini":{requires:["base"]}});
+/*!build time : 2015-01-27 9:57:27 AM*/
+KISSY.config("modules",{"kg/xplayer/2.0.3/mini":{requires:["base"]}});
