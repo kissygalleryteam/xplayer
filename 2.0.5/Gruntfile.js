66823e22655515a3d2e52b8c8a18e3f74daf10df
kissyteam <tiehang.lth@taobao.com>
2015-01-29 16:36:51 +0800

    sync daily version 2.0.5

---
+++ 2.0.5/Gruntfile.js
@@ -0,0 +1,211 @@
+module.exports = function(grunt) {
+    var task = grunt.task;
+    var SRC = './';
+    grunt.initConfig({
+        // 配置文件，参考package.json配置方式，必须设置项是
+        // name, version, author
+        // name作为gallery发布后的模块名
+        // version是版本，也是发布目录
+        // author必须是{name: "xxx", email: "xxx"}格式
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*!build time : <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>*/\n',
+
+        // 对build目录进行清理
+        clean: {
+            build: {
+                src: './build/*'
+            },
+            doc: {
+                src: './doc/*'
+            }
+        },
+        // kmc打包任务，默认情况，入口文件是index.js，可以自行添加入口文件，在files下面
+        // 添加
+        kmc: {
+            options: {
+                packages: [{
+                    name: '<%= pkg.name %>',
+                    path: '../'
+                }],
+                depFilePath: 'mods.js',
+                fixModuleName: true,
+                map: [
+                    ["<%= pkg.name %>/", "kg/<%= pkg.name %>/<%= pkg.version %>/"]
+                ]
+            },
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: SRC,
+                    src: ['./*.js', '!./Gruntfile.js'],
+                    dest: 'build/'
+                }]
+            }
+        },
+        // https://www.npmjs.com/package/grunt-replace
+        replace: {
+            dist: {
+                options: {
+                  patterns: [
+                    {
+                      match: /\@VERSION/g,
+                      replacement: '<%= pkg.version %>'
+                    }
+                  ]
+                },
+                files: [
+                  {
+                      expand: true,
+                      flatten: true,
+                      src: ['build/*.js'],
+                      dest: 'build/'
+                    }
+                ]
+            }
+        },
+        /**
+         * 对JS文件进行压缩
+         * @link https://github.com/gruntjs/grunt-contrib-uglify
+         */
+        uglify: {
+            options: {
+                compress: {
+                    global_defs: {
+                        "DEBUG": false
+                    },
+                    drop_console: true,
+                    dead_code: true
+                },
+                banner: '<%= banner %>',
+                beautify: {
+                    ascii_only: true
+                }
+            },
+            page: {
+                files: [{
+                    expand: true,
+                    cwd: './build',
+                    src: ['**/*.js', '!**/*-min.js'],
+                    dest: './build',
+                    ext: '-min.js'
+                }]
+            }
+        },
+        less: {
+            options: {
+                paths: './'
+            },
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: SRC,
+                    src: ['**/*.less',
+                        '!node_modules/**/*.less',
+                        '!templates/**/*.less',
+                        '!build/**/*.less',
+                        '!demo/**/*.less'
+                    ],
+                    dest: './build/',
+                    ext: '.css'
+                }]
+            }
+        },
+        // 拷贝 CSS 文件
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: SRC,
+                    src: [
+                        '**/*.css',
+                        '!node_modules/**/*.css',
+                        '!build/**/*.css',
+                        '!demo/**/*.css',
+                        '!templates/**/*.css',
+                        '!doc/**/*.css'
+                    ],
+                    dest: './build/',
+                    filter: 'isFile'
+                }, {
+                    expand: true,
+                    cwd: 'flash',
+                    flatten: true,
+                    src: '**/*.swf',
+                    dest: './build/',
+                    filter: 'isFile'
+                }]
+            }
+        },
+        // 监听JS、CSS、LESS文件的修改
+        watch: {
+            'all': {
+                files: [
+                    './src/**/*.css',
+                    '!./build/**/*'
+                ],
+                tasks: ['build']
+            }
+        },
+        cssmin: {
+            scss: {
+                files: [{
+                    expand: true,
+                    cwd: './build',
+                    src: ['**/*.scss.css', '!**/*.scss-min.css'],
+                    dest: './build',
+                    ext: '.scss-min.css'
+                }]
+            },
+            less: {
+                files: [{
+                    expand: true,
+                    cwd: './build',
+                    src: ['**/*.less.css', '!**/*.less-min.css'],
+                    dest: './build',
+                    ext: '.less-min.css'
+                }]
+            },
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: './build',
+                    src: ['**/*.css', '!**/*-min.css', '!**/*.less.css', '!**/*.scss.css', '!doc/**/*.css', '!templates/**/*.css'],
+                    dest: './build',
+                    ext: '-min.css'
+                }]
+            }
+        },
+        jsdoc: {
+            dist: {
+                src: [
+                    'index.js',
+                    'plugin/status.js',
+                    'README.md'
+                ],
+                options: {
+                    'private': false,
+                    'destination': './doc',
+                    'configure': 'templates/jaguar/conf.json',
+                    'template': 'templates/jaguar'
+                }
+            }
+        }
+    });
+
+    // 使用到的任务，可以增加其他任务
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+
+    grunt.registerTask('build', '默认构建任务', function() {
+        task.run(['clean:build', 'kmc', 'replace', 'uglify', 'copy', 'less', 'cssmin']);
+    });
+
+    grunt.registerTask('doc', 'js', function() {
+        task.run(['clean:doc', 'jsdoc']);
+    });
+    return grunt.registerTask('default', '', function(type) {
+        if (!type) {
+            task.run(['build']);
+        }
+    });
+};
---
+++ 2.0.5/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2015-01-29 4:15:55 PM*/
+KISSY.config("modules",{"kg/xplayer/2.0.4/mini":{requires:["base"]}});
