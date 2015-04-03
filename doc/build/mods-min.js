b15f2d383469ff55336022623343bc21c599f3e3
kissyteam <tiehang.lth@taobao.com>
2015-04-03 13:25:26 +0800

    sync daily version 2.1.0

--- doc/build/mods-min.js
+++ doc/build/mods-min.js
@@ -1,2 +1,1 @@
-/*!build time : 2015-01-27 9:57:27 AM*/
-KISSY.config("modules",{"kg/xplayer/2.0.3/mini":{requires:["base"]}});
+KISSY.config("requires",{"kg/xplayer/2.1.0/index":["./plugin/audio","./plugin/audioSwf"],"kg/xplayer/2.1.0/mini":["./plugin/audio"],"kg/xplayer/2.1.0/plugin/audio":["base","./status"],"kg/xplayer/2.1.0/plugin/audioSwf":["base","swf","./status"]});
